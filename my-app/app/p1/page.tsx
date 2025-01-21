04920fbe3f9142a2cbf0e923ce8f93e9d441b9c6
Peera-27 <crbrsonline@gmail.com>
2025-01-21 10:29:56 +0700

    comit1

--- my-app/app/p1/page.tsx
+++ my-app/app/p1/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
     return (
         <div>
-            <h1 className="text-3xl text-center">Page 1</h1>
+            <h1 className="text-3xl text-center text-red-500">Page 1</h1>
             <Image src="/img1.jpg" width={300} height={300} alt="img1" className="mx-auto items-center"></Image>
     <Link href="/p1" className="text-center">Goto Pages1</Link><br />
     <Link href="/p2" className="text-center">Goto Pages2</Link><br />
